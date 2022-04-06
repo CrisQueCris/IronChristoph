c656f66c0c043c2905a57bc2bae13b236694f410
Christoph Rendel <c.rendel@posteo.de>
2022-04-06 13:41:54 +0200

    finished lab
    without optional

--- Lab SQL/Lab SQL Subqueries.sql
+++ Lab SQL/Lab SQL Subqueries.sql
@@ -78,13 +78,25 @@ SELECT first_name, last_name, email
 		JOIN city
 			ON city.city_id = address.city_id
 		JOIN country
-			ON country.country_id = city.city_id
-	WHERE country.country = 'CANADA';
-
-
+			ON country.country_id = city.country_id
+		 WHERE country.country = 'CANADA';
 
 
 -- # Optional
--- 6. Which are films starred by the most prolific actor? Most prolific actor is defined as the actor that has acted in the most number of films. First you will have to find the most prolific actor and then use that actor_id to find the different films that he/she starred.
--- 7. Films rented by most profitable customer. You can use the customer table and payment table to find the most profitable customer ie the customer that has made the largest sum of payments
+-- 6. Which are films starred by the most prolific actor? 
+-- Most prolific actor is defined as the actor that has acted in the most number of films. 
+-- First you will have to find the most prolific actor and then use that actor_id to find the different films that he/she starred.
+
+
+
+
+
+-- 7. Films rented by most profitable customer. 
+-- You can use the customer table and payment table to find the most profitable customer 
+-- ie the customer that has made the largest sum of payments
+
+
+
+
+
 -- 8. Customers who spent more than the average payments.
